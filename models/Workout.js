6f56b11903d70bdedf95a18a10f0573a688743cc
LinhBui88 <87442413+Linhbui88@users.noreply.github.com>
2021-12-28 14:13:14 -0500

    finished Model

--- models/Workout.js
+++ models/Workout.js
@@ -1,18 +1,54 @@
-const mongoose= require('mongoose')
+const mongoose= require('mongoose');
+const Schema = mongoose.Schema;
+
+
+
+
 const WorkoutSchema = new mongoose.Schema({
-  day:Date,
+  day:{
+    type: Date,
+    default: Date.now
+  },
   exercises :[
     {
-      type: String,
-      name: String,
-      duration: Number,
-      weight: Number,
-      reps: Number,
-      sets: Number
+      type: {
+        type: String,
+        trim: true,
+        required: "Enter excercise type"
+      },
+
+      name:{ 
+        type: String,
+        trim: true,
+        required: "Enter excercise name"
+      },
+     
+      weight: {
+        type: Number
+      },
+      reps: {
+        type: Number
+      },
+      sets: {
+        type: Number
+      },
+      duration: {
+        type: Number,
+        required: "Enter an excercise duration in minutes"
+      },
+      distance: {
+        type: Number
+      }
     }
   ]
 
-})
+});
+
+WorkoutSchema.methods.totalDuration = function() {
+  return this.excercises.reduce((total, excercise) =>{
+    return total + excercise.duration
+  })
+}
 const Workout = mongoose.model('Workout',WorkoutSchema)
 
 module.exports = Workout;
